98237c570b830d54b632937a45366801c806ec5e
Taimoor Ali <taimoorg281@gmail.com>
2024-07-26 12:09:14 +0500

    nothing

--- dwm/config.h
+++ dwm/config.h
@@ -5,13 +5,13 @@
 //#include "/home/taimoor/.cache/wal/colors-wal-dwm.h"
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 2;       /* snap pixel */
-static const unsigned int gappih    = 2;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 2;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 2;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 2;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gappih    = 4;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 4;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 4;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 4;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -27,7 +27,7 @@ static const char col_cyan[]         = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_red },
 };
 
 typedef struct {
@@ -166,9 +166,6 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask, 	XK_e, 		spawn, SHCMD("dm-emoji") },
 	{ MODKEY, 	    	XK_y, 		spawn, SHCMD("dm-yt") },
 	{ MODKEY|ShiftMask,   	XK_y, 		spawn, SHCMD("dm-ytmusic") },
-	{ MODKEY,  XK_n, spawn, {.v = (const char*[]){ TERMINAL, "-e", "nvim", "-c", "VimwikiIndex", NULL } } },
-	{ MODKEY|ShiftMask, XK_d, 
-		spawn, {.v = (const char*[]){ TERMINAL, "-e", "nvim", "-c", "VimwikiDiaryIndex", NULL } } },
 	{ MODKEY|ShiftMask, 	XK_n, spawn, SHCMD(TERMINAL " -e newsboat ; pkill -RTMIN+6 dwmblocks") },
 	{ MODKEY, 	    	XK_b, spawn, SHCMD("brave") },
 
